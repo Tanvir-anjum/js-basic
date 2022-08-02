bc19b851854ce7d5eb9c1699531d6970f304a31a
Tanvir-anjum <80684462+Tanvir-anjum@users.noreply.github.com>
2022-08-02 23:13:25 +0600

    Add files via upload
    js  common methods and number methods

---
+++ basic.js
@@ -0,0 +1,30 @@
+const a = "tanvir"
+let b = 20
+let c = "20.5"
+let d = 20.56575
+
+console.log(typeof(a)) //Used for knowing the data type of variable typeof()
+
+b = toString(b) //Used for coverting the variable data to string toString()
+console.log(typeof(b))
+
+c = parseInt(c) //Used for coverting a string to inside string's exact data type parseInt()
+console.log(typeof(c))
+
+c = parseFloat(c) //Used for coverting a string to number if its a secimal number  parseFloat()
+console.log(c)
+
+/*NUMBER METHOD*/
+
+console.log(d.toFixed(2)) //used for showing how many number wanna show after point(.) toFixed()
+
+console.log(d.toPrecision(2)) //used for showing how many number wanna show in a number data type toPrecision()
+
+console.log(typeof(Number(a))) //used for converting string to number Number()
+
+
+
+
+
+
+
